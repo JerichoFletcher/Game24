7bde0ccd46b64bcec8e2c2c9c8fa279a53a2bb35
JerichoFletcher <jericho.russel17@gmail.com>
2023-01-20 23:29:44 +0700

    Output to file

--- src/game24/Game24.java
+++ src/game24/Game24.java
@@ -1,5 +1,8 @@
 package game24;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import game24.struct.*;
@@ -7,6 +10,8 @@ import game24.util.*;
 
 public class Game24{
     static Scanner in;
+    static ArrayList<Solution> solutions = null;
+    static long elapsedNano;
 
     /* *** MAIN *** */
     public static void main(String[] args){
@@ -17,7 +22,6 @@ public class Game24{
         System.out.println("Kartu merupakan salah satu dari: A 2 3 4 5 6 7 8 9 10 J Q K");
         System.out.println("Atau masukkan input R untuk menggunakan input acak");
         
-        ArrayList<Solution> solutions = null;
         boolean valid;
         do{
             valid = true;
@@ -33,7 +37,7 @@ public class Game24{
                 inpVal = Convert.card4ToValue4(inps);
                 if(inpVal != null){
                     // Input valid, proses
-                    solutions = Solver.solve(inpVal);
+                    startSolve(inpVal);
                 }else{
                     // Input tidak valid
                     valid = false;
@@ -46,7 +50,7 @@ public class Game24{
                 System.out.println(String.join(" ", inps));
 
                 inpVal = Convert.card4ToValue4(inps);
-                solutions = Solver.solve(inpVal);
+                startSolve(inpVal);
             }
         }while(!valid);
 
@@ -56,17 +60,27 @@ public class Game24{
         // Simpan solusi yang ditemukan
         promptSaveResults();
 
-        System.out.println(String.format("\nProgram selesai dengan waktu eksekusi %dms", 0));
+        // Close screen, tampilkan waktu pencarian
+        System.out.println(String.format("\nProgram selesai dengan waktu pencarian %dms", elapsedNano / 1000000));
         System.out.print("Tekan ENTER untuk menutup program...");
-        in.nextLine();
+        in.nextLine(); // Pause
         in.close();
+    }
+
+    static void startSolve(float[] inpVal){
+        long timeBefore = System.nanoTime();
+        solutions = Solver.solve(inpVal);
+        long timeAfter = System.nanoTime();
+
+        // Pengukuran waktu pencarian
+        elapsedNano = timeAfter - timeBefore;
     }
 
     static void displaySolutions(ArrayList<Solution> solutions){
         if(solutions.size() == 0){
             System.out.println("Tidak ada solusi");
         }else{
-            System.out.println(String.format("Ditemukan %d solusi", solutions.size()));
+            System.out.println(String.format("Ditemukan %d solusi sebagai berikut:", solutions.size()));
             for(Solution sol : solutions){
                 System.out.println(sol.toString());
             }
@@ -81,8 +95,28 @@ public class Game24{
             switch(inp){
                 case "Y":
                     // Simpan file
-                    System.out.println(String.format("Solusi berhasil disimpan di %s", "PATHFILE"));
+                    System.out.print("Nama file: ");
+                    String filename = String.format("test/%s", in.nextLine());
+                    if(!filename.endsWith(".txt"))filename = String.format("%s.txt", filename);
+
+                    BufferedWriter writer = null;
+                    try{
+                        writer = new BufferedWriter(new FileWriter(String.format("../%s", filename)));
+                        for(Solution sol : solutions){
+                            writer.write(sol.toString());
+                            writer.newLine();
+                        }
+                        System.out.println(String.format("Solusi berhasil disimpan di %s", filename));
+                    }catch(IOException e){
+                        System.out.print("ERR: Gagal menyimpan file: ");
+                        System.out.println(e.toString());
+                    }finally{
+                        try{
+                            writer.close();   
+                        }catch(IOException e){}
+                    }
                 case "N":
+                    // Akhiri program
                     valid = true;
                 default:
             }
